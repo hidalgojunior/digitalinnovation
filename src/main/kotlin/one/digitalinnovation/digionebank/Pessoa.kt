f1d5d3097c292750ffda9940c0e5e2accdee3cd2
Arnaldo Martins Hidalgo Junior <arnaldo.hidalgo@etec.sp.gov.br>
2021-07-16 14:55:00 -0300

    Atualizando a propriedade cpf para modificador SET

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,10 +4,14 @@ class Pessoa {
     var nome : String = "Arnaldo"
     var cpf : String = "123.123.123-11"
 
+    private set
 }
 fun main(){
     val arnaldo = Pessoa()
 
+
+    println(arnaldo)
     println(arnaldo.nome)
     println(arnaldo.cpf)
+
 }
